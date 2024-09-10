6a08014305da6613f32ae5c5d35c3c01dc4f4a50
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-09-10 10:28:27 +0200

    done

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ const result = document.getElementById("result");
 let temp;
 
 function convert() {
-    
+
     if (toFarhenheit.checked) {
-    
+        temp = NUmber(textbox.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + "°F";
+
     } else if (toCelsius.checked) {
-    
+        temp = NUmber(textbox.value);
+        temp = (temp - 32) * (5/9);
+        result.textContent = temp.toFixed(1) + "°C";
     } else {
         result.textContent = "Select a unit";
     }
