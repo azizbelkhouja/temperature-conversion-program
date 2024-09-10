946fc04667d3e7c9780dd434891625015f035dca
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-09-10 10:19:42 +0200

    js

---
+++ index.js
@@ -0,0 +1,5 @@
+const textbox = document.getElementById("textbox");
+const toFarhenheit = document.getElementById("toFarhenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+let temp;
