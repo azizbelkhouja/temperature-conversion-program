32c87c9b3399bc21134f519b0e7dfeef8c0d70cb
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-09-10 10:21:57 +0200

    convert function

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const toFarhenheit = document.getElementById("toFarhenheit");
 const toCelsius = document.getElementById("toCelsius");
 const result = document.getElementById("result");
 let temp;
+
+function convert() {
+    
+    if (toFarhenheit.checked) {
+    
+    } else if (toCelsius.checked) {
+    
+    } else {
+        result.textContent = "Select a unit";
+    }
+
+}
